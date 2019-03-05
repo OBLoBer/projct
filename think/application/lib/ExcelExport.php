3ae86257b01e9853834b7be4a39bf7fd2e028f3a
huangjie <1605805630@qq.com>
2019-03-05 18:25:48 +0800

    hj

--- think/application/lib/ExcelExport.php
+++ think/application/lib/ExcelExport.php
@@ -7,198 +7,51 @@
  */
 namespace app\lib;
 
-
-class ExcelExport
-{
-    //字段对应的标题
-    private $title = [];
-    //文件名
-    private $filename = '';
-    //字段值过滤器
-    private $filter = [];
-    //存储文件的临时目录
-    private $stodir = '/tmp/';
-
-    /**
-     * 指定临时存储路径
-     * 请确定这个路径有读写权限
-     */
-    public function tmpdir(string $dir)
-    {
-        if(substr($dir, -1) != '/'){
-            $dir .= '/';
-        }
-        $this->stodir = $dir;
-        return $this;
+function ExcelExport($Model,$where){
+    ob_end_clean();
+    header("Content-type:application/vnd.ms-excel;charset=UTF-8");
+    header("Cache-Control:must-revalidate, post-check=0, pre-check=0");
+    header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+    header("Content-Type:application/force-download");
+    header("Content-Type:application/vnd.ms-execl");
+    header("Content-Type:application/octet-stream");
+    header("Content-Type:application/download");;
+    header('Content-Disposition:attachment;filename="入库明细表.xls"');
+    header("Content-Transfer-Encoding:binary");
+    $innerdata = Model($Model)->where($where)->select();
+    $table = '';
+    $table .= "'<html xmlns:o=\"urn:schemas-microsoft-com:office:office\" xmlns:x=\"urn:schemas-microsoft-com:office:excel\" xmlns=\"http://www.w3.org/TR/REC-html40\">
+                        <!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">
+                        <html>
+                             <head>
+                                <meta http-equiv=\"Content-type\" content=\"text/html;charset=UTF-8\" />
+                                 <style id=\"Classeur1_16681_Styles\"></style>
+                             </head>
+                             <body>
+                                    <table>
+                                        <thead>
+                                            <tr>
+                                                <th class='name'>名称</th>
+                                                <th class='name'>入库日期</th>
+                                                <th class='name'>入库库位</th>
+                                                <th class='name'>供货商</th>
+                                                <th class='name'>入库人</th>
+                                                <th class='name'>数量</th>
+                                                <th class='name'>单价</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>";
+    foreach ($innerdata as $v) {
+        $table .= "<tr>
+                                                <td class='name'>{$v['phone']}</td>
+                                                <td class='name'>{$v['password']}</td>
+                                                <td class='name'>{$v['qq']}</td>
+                                                <td class='name'>{$v['cash']}</td>
+                                                <td class='name'>{$v['phone']}</td>
+                                                <td class='name'>{$v['point']}</td>
+                                                <td class='name'>{$v['time']}</td>
+                                            </tr>";
     }
-
-    /**
-     * 生成 excel 数据表文件
-     * @param  array  $data 要导出的数据
-     * @return bool
-     */
-    public function excel(array $data=[], $i=1)
-    {
-        set_time_limit(0);
-        header("Content-type: text/html; charset=utf-8");
-        if($data && is_array($data)){
-            $filename = $this->filename ? $this->filename : date('Y_m_d');
-            $filter = $this->filter;
-            $current = (array)current($data);
-            if(is_array($current)){
-                $filePath = $this->stodir . $filename . "($i)" . '.csv';
-                $fp = fopen($filePath, 'a');
-                $columns = $this->titleColumn(array_keys($current));
-                fputcsv($fp, $columns);
-                foreach ($data as &$row) {
-                    foreach ($row as $k => &$v) {
-                        if(!isset($columns[$k])){
-                            unset($row[$k]);
-                        }
-                        if(isset($filter[$k])){
-                            if($filter[$k]=='datetime'){
-                                $v = date("Y-m-d H:i:s",$v);
-                            }
-                            if($filter[$k]=='date'){
-                                $v = date("Y-m-d",$v);
-                            }
-                            if(is_array($filter[$k])){
-                                $v = isset($filter[$k][$v]) ? $filter[$k][$v] : $v;
-                            }
-                        }
-                    }
-                    fputcsv($fp, $row);
-                }
-                fclose($fp);
-                unset($data);
-                return true;
-            }
-        }
-        return false;
-    }
-
-    /**
-     * 打包好zip文件并导出
-     * @param  [type] $filename [description]
-     * @return [type]           [description]
-     */
-    public function fileload()
-    {
-        $zipname = $this->stodir . '.zip';
-        $zipObj = new ZipArchive();
-        if($zipObj->open($zipname, ZipArchive::CREATE) === true){
-            $res = false;
-            foreach(glob($this->stodir . "*") as $file){
-                $res = $zipObj->addFile($file);
-            }
-            $zipObj->close();
-            if($res){
-                header ("Cache-Control: max-age=0");
-                header("Content-Description: File Transfer");
-                header("Content-Disposition: attachment;filename =" . $zipname);
-                header('Content-Type: application/zip');
-                header('Content-Transfer-Encoding: binary');
-                header ('Content-Length: ' . filesize($zipname));
-
-                @readfile($zipname);//输出文件;
-                //清理临时目录和文件
-                $this->deldir($this->stodir);
-                @unlink($zipname);
-                ob_flush();
-                flush();
-            }else{
-                $this->deldir($this->stodir);
-                ob_flush();
-                flush();
-                die('暂无文件可下载！');
-            }
-        }else{
-            $this->deldir($this->stodir);
-            ob_flush();
-            flush();
-            die('文件压缩失败！');
-        }
-        exit();
-    }
-
-    /**
-     * 清理目录，删除指定目录下所有内容及自身文件夹
-     * @param  [type] $dir [description]
-     * @return [type]       [description]
-     */
-    private function deldir($dir)
-    {
-        if(is_dir($dir)){
-            foreach(glob($dir . '*') as $file){
-                if(is_dir($file)) {
-                    deldir($file);
-                    @rmdir($file);
-                } else {
-                    @unlink($file);
-                }
-            }
-            @rmdir($dir);
-        }
-    }
-
-    /**
-     * 设置标题
-     * @param array $title 标题参数为字段名对应标题名称的键值对数组
-     * @return obj this
-     */
-    public function title($title)
-    {
-        if($title && is_array($title)){
-            $this->title = $title;
-        }
-        return $this;
-    }
-
-    /**
-     * 设置导出的文件名
-     * @param string $filename 文件名
-     * @return obj this
-     */
-    public function filename($filename)
-    {
-        $this->filename = date('Y_m_d') . (string)$filename;
-        if(!is_dir($this->stodir . $this->filename)){
-            mkdir($this->stodir . $this->filename);
-        }
-        $this->stodir .= $this->filename . '/';
-        return $this;
-    }
-
-    /**
-     * 设置字段过滤器
-     * @param array $filter 文件名
-     * @return obj this
-     */
-    public function filter($filter)
-    {
-        $this->filter = (array)$filter;
-        return $this;
-    }
-
-    /**
-     * 确保标题字段名和数据字段名一致,并且排序也一致
-     * @param  array $keys  要显示的字段名数组
-     * @return array 包含所有要显示的字段名的标题数组
-     */
-    protected function titleColumn(array $keys)
-    {
-        $title = $this->title;
-        if($title && is_array($title)){
-            $titleData = [];
-            foreach ($keys as $v) {
-                if(isset($title[$v])){
-                    $titleData[$v] = $title[$v];
-                    unset($title[$v]);
-                }
-            }
-            unset($keys);
-            return $titleData;
-        }
-        return $keys;
-    }
+    $table .= "</tbody></table></body></html>";
+    echo $table;
 }
--- think/application/user/controller/Index.php
+++ think/application/user/controller/Index.php
@@ -9,14 +9,73 @@
 namespace app\user\controller;
 
 
+use app\extra\ChinesePinyin;
+use think\Db;
+
 class Index extends Base
 {
+
     public function UserData(){
-//        if(!request()->isPost()){
-//            return lang('Request type error');
-//        }
-        $User_id = Session('name')['id'];
-        $row = Model('UserUser')->find();
-        dump($row);
-    }
+            ob_end_clean();
+            header("Pragma: public");
+            header("Expires: 0");
+            header("Content-Type:application/vnd.ms-excel;charset=UTF-8");
+            header("Content-Type:application/vnd.ms-excel;");
+            header("Cache-Control:must-revalidate, post-check=0, pre-check=0");
+            header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+            header("Content-Type:application/force-download");
+            header("Content-Type:application/vnd.ms-execl");
+            header("Content-Type:application/octet-stream");
+            header("Content-Type:application/download");;
+            header('Content-Disposition:attachment;filename="入库明细表.xls"');
+            header("Content-Transfer-Encoding:binary");
+            $start_date = input('start');
+            $end_date = input('end');
+            $innerdata = Model('UserUser')
+                ->where('id','>',0)
+                 ->select();
+
+            $table = '';
+            $table .= "'<html xmlns:o=\"urn:schemas-microsoft-com:office:office\" xmlns:x=\"urn:schemas-microsoft-com:office:excel\" xmlns=\"http://www.w3.org/TR/REC-html40\">
+                        <!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">
+                        <html>
+                             <head>
+                                <meta http-equiv=\"Content-type\" content=\"text/html;charset=UTF-8\" />
+                                 <style id=\"Classeur1_16681_Styles\"></style>
+                             </head>
+                             <body>
+                                    <table >
+                                        <thead>
+                                            <tr style='border: 1px solid black;background-color: lightblue'>
+                                                <th class='name'>名称</th>
+                                                <th class='name'>入库日期</th>
+                                                <th class='name'>入库库位</th>
+                                                <th class='name'>供货商</th>
+                                                <th class='name'>入库人</th>
+                                                <th class='name'>数量</th>
+                                                <th class='name'>单价</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>";
+                                        foreach ($innerdata as $v) {
+                                        $table .= "<tr  style='border: 1px solid black;'>
+                                                <td class='name'>{$v['phone']}</td>
+                                                <td class='name'>{$v['password']}</td>
+                                                <td class='name'>{$v['qq']}</td>
+                                                <td class='name'>{$v['cash']}</td>
+                                                <td class='name'>{$v['phone']}</td>
+                                                <td class='name'>{$v['point']}</td>
+                                                <td class='name'>{$v['time']}</td>
+                                            </tr>";
+            }
+            $table .= "</tbody></table></body></html>";
+            echo $table;
+        }
+        public function oExcel(){
+                $innerdata = Model('UserUser')
+                    ->where('id','>',0)
+                    ->select();
+                $Excel = new \PHPExcel();
+                dump($Excel);
+        }
 }
